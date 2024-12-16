0dff86ea6c92b70fd1f83ab11d394708a6508b31
Apham130 <andypham4747@gmail.com>
2024-12-16 21:49:24 +0000

    fixed inputs 1 and 2 a bit

--- calculator.cpp
+++ calculator.cpp
@@ -8,37 +8,38 @@ int main()
 {
   std::srand(std::time(0));  // seeding our random number generator
 
-  int user_input = 0;
-  int num1;
-  int num2;
-  int numerator;
-  int denominator;
+
+  std::cout << "Welcome to C++ Calculator!  Choose an option below!\n";
+
+  int user_input;
   while (user_input != 99)
   {
-    std::cout << "Welcome to C++ Calculator!  Choose an option below!\n"
-              << "0. Roll a dice\n"        
+    std::cout << std::endl
+              << "0. Random Number Generator\n"        
               << "1. Add\n"
               << "2. Subtract\n"
               << "3. Multiply\n"
               << "4. Divide\n"
               << "5. Square root\n"
               << "99. Quit\n\n> ";
-
     std::cin >> user_input;
-    
-    // implement calculator below
-    if (user_input == 0)
+
+    if (user_input == 0) //Roll random numberr
     {
+      int bounds;
+      std::cout << "Pick the bounds (any n# >1)"
       int random_number = (std::rand() % 6) + 1;  // random numbers between 1 and 6
-      std::cout << "You rolled a " << random_number << std::endl;
+      std::cout << "\nYou rolled a " << random_number << "!\n";
     }
     else
     {
       if (user_input == 1)
       {
+        int num1;
+        int num2;
         std::cout << "Enter two numbers\n";
         std::cin >> num1 >> num2;
-        std::cout << "Their sum is " << (num1 + num2) << std::endl;
+        std::cout << "\nTheir sum is " << (num1 + num2) << "!\n";
       }
     }
   }
